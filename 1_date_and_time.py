ae0e93bed026e88e574c6a58eaf446ee9be93ccf
SanuNak <sanu.nak.@gmail.com>
2021-11-30 19:48:30 +0300

    Выполнено Домашнее задание №2

--- 1_date_and_time.py
+++ 1_date_and_time.py
@@ -7,21 +7,22 @@
 2. Превратите строку "01/01/20 12:10:03.234567" в объект datetime
 
 """
+import datetime
 
 def print_days():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+
+    delta = datetime.timedelta(days=1)
+    now_date = datetime.date.today()
+    yesterday = now_date - delta
+    month_later = now_date - datetime.timedelta(days=30)
+
+    print(f'вчера: {yesterday}, сегодня: {now_date}, 30 дней назад: {month_later}')
 
 
 def str_2_datetime(date_string):
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+    str_2_time = datetime.datetime.strptime(date_string, "%d/%m/%y %H:%M:%S.%f")
+    return str_2_time
+
 
 if __name__ == "__main__":
     print_days()
--- 3_dict_to_csv.py
+++ 3_dict_to_csv.py
@@ -1,5 +1,4 @@
 """
-
 Домашнее задание №2
 
 Работа csv
@@ -9,13 +8,20 @@
 2. Запишите содержимое списка словарей в файл в формате csv
 
 """
-
+import csv
 def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+    dict_name = [
+        {'name': 'Маша', 'age': 25, 'job': 'Scientist'},
+        {'name': 'Вася', 'age': 8, 'job': 'Programmer'},
+        {'name': 'Эдуард', 'age': 48, 'job': 'Big boss'},
+    ]
+
+    with open('download/export.csv', 'w', encoding='utf-8', newline='') as f:
+        fields = ['name', 'age', 'job']
+        writer = csv.DictWriter(f, fields, delimiter=';')
+        writer.writeheader()
+        for user in dict_name:
+            writer.writerow(user)
 
 if __name__ == "__main__":
     main()
